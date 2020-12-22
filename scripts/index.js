98d1c93655ff0c644b32baee48909d69e9b7c2c4
mauricio <mauris_amigos@hotmail.com>
2020-12-22 10:19:18 -0600

    Separando el menu plegable de el header

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,10 @@
-const $button = document.querySelector('.header__button')
-const $plegableMenu = document.querySelector('.header__menu--mobile')
+const $button = document.querySelector('.hamburger')
+const $plegableMenu = document.querySelector('.plegable-menu')
 
 document.addEventListener('click', e => {
-	if (e.target.matches('.header__button')) {
+	console.log(e.target)
+	if (e.target.matches('.hamburger') || e.target.matches('.hamburger *')) {
 		$plegableMenu.classList.toggle('active')
+		$button.classList.toggle('is-active')
 	}
 })
