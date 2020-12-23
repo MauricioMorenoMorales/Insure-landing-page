4a3464327d20dbd8fda8c5a731a72d2bc4cbbb22
mauricio <mauris_amigos@hotmail.com>
2020-12-23 12:01:53 -0600

    Header y hero completado

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,15 @@
 const $button = document.querySelector('.hamburger')
 const $plegableMenu = document.querySelector('.plegable-menu')
+const $backgroundImage = document.querySelector(
+	'.hero-container__background-image--right--mobile',
+)
 
 document.addEventListener('click', e => {
 	console.log(e.target)
 	if (e.target.matches('.hamburger') || e.target.matches('.hamburger *')) {
 		$plegableMenu.classList.toggle('active')
 		$button.classList.toggle('is-active')
+		$backgroundImage.classList.toggle('active')
 	}
 	if (e.target.matches('.nav-link')) {
 		$plegableMenu.classList.remove('active')
