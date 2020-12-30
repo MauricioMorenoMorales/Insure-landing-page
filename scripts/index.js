96ef6dd37860230128d2899d81991abff7917603
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2020-12-29 18:39:07 -0600

    Iphone 5 landscape bug fixed

--- scripts/index.js
+++ scripts/index.js
@@ -3,9 +3,9 @@ const $plegableMenu = document.querySelector('.plegable-menu')
 const $backgroundImage = document.querySelector(
 	'.hero-container__background-image--right--mobile',
 )
+const $navLink = document.querySelectorAll('.nav-li')
 
 document.addEventListener('click', e => {
-	console.log(e.target)
 	if (e.target.matches('.hamburger') || e.target.matches('.hamburger *')) {
 		$plegableMenu.classList.toggle('active')
 		$button.classList.toggle('is-active')
@@ -14,5 +14,19 @@ document.addEventListener('click', e => {
 	if (e.target.matches('.nav-link')) {
 		$plegableMenu.classList.remove('active')
 		$button.classList.remove('is-active')
+	}
+})
+
+window.addEventListener('resize', () => {
+	if (window.innerHeight < 413 && window.innerWidth < 650) {
+		$navLink.forEach(e => {
+			e.style.marginTop = '10px'
+			e.style.padding = '0'
+		})
+	} else {
+		$navLink.forEach(e => {
+			e.style.marginTop = '10px'
+			e.style.padding = '20px'
+		})
 	}
 })
